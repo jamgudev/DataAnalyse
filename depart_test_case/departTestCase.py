b4d9d55e1d7642846434a983727ea7c29ae11103
jamgudev <826630153@qq.com>
2023-06-25 18:41:19 +0800

    feat:Initial commit

---
+++ depart_test_case/departTestCase.py
@@ -0,0 +1,35 @@
+import os.path
+
+import pandas as pd
+
+io = "./depart_test_case/input/all.xlsx"  # 目标文件
+data = pd.read_excel(io, header=None)
+
+rows = data.shape[0]  # 获取行数 shape[1]获取列数
+cols_list = []
+test_rows = []
+training_rows = []
+
+for i in range(rows):
+    temp = data.iloc[i]
+
+    if len(temp) == 0:
+        continue
+
+    if (i % 4) == 0:
+        test_rows.append(temp)
+    else:
+        training_rows.append(temp)
+
+# 输出test数据
+outDir = "./depart_test_case/out/"
+testDir = outDir + "all_test.xlsx"
+trainDir = outDir + "all_train.xlsx"
+print(testDir)
+print(trainDir)
+if not os.path.exists(outDir):
+    os.makedirs(outDir)
+testDF = pd.DataFrame(test_rows)
+testDF.to_excel(testDir, header=None, index=False)
+trainDF = pd.DataFrame(training_rows)
+trainDF.to_excel(trainDir, header=None, index=False)
